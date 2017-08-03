6044555f67e799c44663ac3b0fb20757399d0977
perlancar (on pc-office) <perlancar@gmail.com>
2017-08-03 16:15:18 +0700

    Initial commit

---
+++ lib/Log/ger/Level/Like/RubyLogging.pm
@@ -0,0 +1,49 @@
+package Log::ger::Level::Like::RubyLogging;
+
+# DATE
+# VERSION
+
+use Log::ger ();
+
+%Log::ger::Levels = (
+    unknown => 0,
+    fatal   => 1,
+    error   => 2,
+    warn    => 3,
+    info    => 4,
+    debug   => 5,
+);
+
+%Log::ger::Level_Aliases = (
+    off => 0,
+);
+
+1;
+# ABSTRACT: Set default log level from some environment variables
+
+=head1 SYNOPSIS
+
+ # load before 'use Log::ger' in any package/target
+ use Log::ger::Level::Like::RubyLogging;
+
+
+=head1 DESCRIPTION
+
+From the documentation of the Logging library [1]: "Logging is a flexible
+logging library for use in Ruby programs based on the design of Java's log4j
+library. It features a hierarchical logging system, custom level names, multiple
+output destinations per log event, custom formatting, and more."
+
+This module changes the L<Log::ger> levels to:
+
+ unknown (0)
+ fatal (1)
+ error (2)
+ warn (3)
+ info (4)
+ debug (5)
+
+
+=head1 SEE ALSO
+
+[1] L<http://www.rubydoc.info/gems/logging/file/README.md>
